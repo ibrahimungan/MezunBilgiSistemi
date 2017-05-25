9ce48d4359ec044b342278a1d2b681bf87ae3b3d
ibojino <ibojino@gmail.com>
2017-05-26 02:34:29 +0300

    HashTable ADT eklendi

---
+++ MezunBilgiSistemi/ADT/HashTable/HashTableADT.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MezunBilgiSistemi.Models;
+using MezunBilgiSistemi.ADT.Heap;
+
+namespace MezunBilgiSistemi.ADT.HashTable
+{
+    public class HashTableADT
+    {
+        Dictionary<string, HeapADT> bolumlerTablosu;
+        public HashTableADT()
+        {
+            bolumlerTablosu = new Dictionary<string, HeapADT>();
+        }
+        public HeapADT BolumHeapiniGetir(string bolumAdi)
+        {
+            return bolumlerTablosu[bolumAdi];
+        }
+        public void BolumEkle(string bolumAdi, HeapADT bolumHeap)
+        {
+            this.bolumlerTablosu[bolumAdi] = bolumHeap;
+        }
+        public void BolumSil(string bolumAdi)
+        {
+            this.bolumlerTablosu.Remove(bolumAdi);
+        }
+    }
+}
