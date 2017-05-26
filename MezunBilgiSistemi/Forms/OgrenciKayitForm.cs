283cba6837599d0b33bd0a3c1d71be5ccac3fe2e
Tugrul Can <tugrulcansollu@gmail.com>
2017-05-26 11:14:35 +0300

    mezunlistesi.txt eklendi

---
+++ MezunBilgiSistemi/Forms/OgrenciKayitForm.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MezunBilgiSistemi.Forms
+{
+    public partial class frmKayit : Form
+    {
+        public frmKayit()
+        {
+            InitializeComponent();
+        }
+
+        public static DataTable staj = new DataTable();
+
+        private void frmKayit_Load(object sender, EventArgs e)
+        {
+            dataGridStaj.DataSource = staj;
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            frmStajEkle se = new frmStajEkle();
+            se.Show();
+            this.Hide();
+        }
+    }
+}
